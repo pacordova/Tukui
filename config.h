bf4744e801e0cbad7ac33bae1dd721600ed5c352
Paul Augustine Cordova <royalcordova@protonmail.ch>
2018-06-08 16:42:26 -0400

    Font change to Iosevka

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Fira Code:size=10:antialias=true:autohint=true";
+static char font[] = "Iosevka:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
